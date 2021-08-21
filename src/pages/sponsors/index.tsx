1809e33eb01a27564086ddccb334bc520076095f
Sam Hirst <SamuelJHirst@outlook.com>
2021-08-21 22:53:40 +0100

    create basic sponsor page structure

--- src/pages/sponsors/index.tsx
+++ src/pages/sponsors/index.tsx
@@ -1,5 +1,24 @@
-import { Typography } from "@material-ui/core";
+import { 
+  Grid,
+  Typography
+} from "@material-ui/core";
+import { useState } from 'react';
 
 export default function Sponsors() {
-  return <Typography>Sponsors</Typography>;
+  return (
+    <div id="Sponsors">
+      <Typography variant="h3">Sponsors</Typography>
+      <Grid container>
+        <Grid item xs={12} lg={4}>
+          <Typography variant="h4" align="center">Gold</Typography>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Typography variant="h4" align="center">Silver</Typography>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Typography variant="h4" align="center">Bronze</Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
