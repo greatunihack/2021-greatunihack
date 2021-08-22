26fa386e90c4bf01bdd2bafd7eb816dc1c24e1c8
Aryan Agrawal <ary4n99@gmail.com>
2021-08-22 16:56:17 +0200

    Use Link for dashboard and landing page buttons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,32 @@
 import { Button, Grid, Box } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
       <Box m={2}>
         <Box m={4}>
-          <Button variant="contained" href="/dashboard/profile">
+          <Button variant="contained" component={Link} to="/dashboard/profile">
             Profile
           </Button>
         </Box>
         <Box m={4}>
-          <Button variant="contained" href="/dashboard/submissions">
+          <Button variant="contained" component={Link} to="/dashboard/submissions">
             Submissions
           </Button>
         </Box>
         <Box m={4}>
-          <Button variant="contained" href="/dashboard/sponsors">
+          <Button variant="contained" component={Link} to="/dashboard/sponsors">
             Sponsors
           </Button>
         </Box>
         <Box m={4}>
-          <Button variant="contained" href="/dashboard/contact">
+          <Button variant="contained" component={Link} to="/dashboard/contact">
             Contact
           </Button>
         </Box>
         <Box m={4}>
-          <Button variant="contained" href="/dashboard/challenges">
+          <Button variant="contained" component={Link} to="/dashboard/challenges">
             Challenges
           </Button>
         </Box>
