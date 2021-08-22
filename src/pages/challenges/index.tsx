9afccfef604ccaeaa7ac2c80438b8ae559201dba
Sam Hirst <SamuelJHirst@outlook.com>
2021-08-22 23:27:28 +0100

    create basic challenge page structure

--- src/pages/challenges/index.tsx
+++ src/pages/challenges/index.tsx
@@ -1,5 +1,34 @@
-import { Typography } from "@material-ui/core";
+import { 
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+    Typography
+} from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function Challenges() {
-  return <Typography>Challenges</Typography>;
+  return (
+    <>
+      <Box m={2}>
+          <Typography variant="h3">Challenges</Typography>
+      </Box>
+      <Box m={2}>
+          <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                  <Typography>Challenge 1</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                  <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+                  </Typography>
+              </AccordionDetails>
+          </Accordion>
+      </Box>
+    </>
+  );
 }
