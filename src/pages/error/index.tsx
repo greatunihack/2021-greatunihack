072ea079358ce28b14a74c15c6db4742bcadafeb
Sam Hirst <SamuelJHirst@outlook.com>
2021-08-24 18:48:32 +0100

    generate random hackathon tip on 404 page

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -2,6 +2,7 @@ import {
     Box,
     Typography
 } from "@material-ui/core";
+import suggestions from 'src/pages/error/Suggestions.json';
 
 interface ErrorProps {
     code?: number;
@@ -10,10 +11,16 @@ interface ErrorProps {
 
 export default function Error(props: ErrorProps) {
     const { code, message } = props;
+
+    const index = Math.floor(Math.random() * suggestions.length); // Generates random number between 0 and one less than the number of suggestions
+    const tip = suggestions[index];
+
     return (
         <Box m={2}>
             <Typography variant="h1">{code ?? 400}</Typography>
             <Typography variant="h4">{message ?? "Something Went Wrong"}</Typography>
+            <Typography variant="h5">Hackathon Tip {index + 1}</Typography>
+            <Typography>{tip}</Typography>
         </Box>
     );
 }
