639e4edc0fa782e4b13c89214c905133f8f4fcf9
Varun Shankar <varunshankar02@gmail.com>
2021-08-30 17:52:00 +0100

    navbar added

---
+++ src/components/landing/navbar.tsx
@@ -0,0 +1,45 @@
+import {
+  Button,
+  Grid,
+  Toolbar,
+  Container,
+  Box,
+  AppBar,
+  Typography,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+
+  title: {
+    flewGrow: 1,
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            &#60;GreatUniHack2021/&#62;
+          </Typography>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/apply">
+            Apply
+          </Button>
+          <Button color="inherit" component={Link} to="/dashboard/home">
+            Dashboard
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
