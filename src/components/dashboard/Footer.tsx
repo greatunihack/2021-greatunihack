4207be32af081b29bc8cf0d20460293d3d8709cb
Varun Shankar <varunshankar02@gmail.com>
2021-08-31 21:22:49 +0100

    added social media icons to footer

--- src/components/dashboard/Footer.tsx
+++ src/components/dashboard/Footer.tsx
@@ -1,9 +1,14 @@
-import { AppBar, Link, Typography, makeStyles } from "@material-ui/core";
+import { AppBar, Link, Typography, makeStyles, Grid } from "@material-ui/core";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
     padding: theme.spacing(3, 2),
     marginTop: "auto",
+  },
+  icons: {
+    padding: "3px",
   },
 }));
 
@@ -12,13 +17,34 @@ export default function Footer() {
 
   return (
     <AppBar position="static" className={classes.footer}>
-      <Typography variant="body2">
-        {"Copyright © "}
-        <Link color="inherit" href="https://unicsmcr.com/">
-          UniCS
-        </Link>{" "}
-        {new Date().getFullYear()}
-      </Typography>
+      <Grid justifyContent="space-between" container spacing={10}>
+        <Grid item>
+          <Typography variant="body2">
+            {"Copyright © "}
+            <Link color="inherit" href="https://unicsmcr.com/">
+              UniCS
+            </Link>{" "}
+            {new Date().getFullYear()}
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <div>
+            <InstagramIcon
+              className={classes.icons}
+              onClick={(event) =>
+                (window.location.href = "https://www.instagram.com/unicsmcr/")
+              }
+            />
+            <FacebookIcon
+              className={classes.icons}
+              onClick={(event) =>
+                (window.location.href = "https://www.facebook.com/")
+              }
+            />
+          </div>
+        </Grid>
+      </Grid>
     </AppBar>
   );
 }
