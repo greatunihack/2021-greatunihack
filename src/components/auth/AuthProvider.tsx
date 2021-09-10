48144d730612dd7ce62785d0aeb719e7db5f232e
Aryan Agrawal <ary4n99@gmail.com>
2021-09-10 11:17:24 +0200

    Use env vars for firebase config

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -11,12 +11,12 @@ import {
 } from "firebase/auth";
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDiAu-TMx91OZd3yRuwThXJN0srD2dbaQ4",
-  authDomain: "greatunihack21.firebaseapp.com",
-  projectId: "greatunihack21",
-  storageBucket: "greatunihack21.appspot.com",
-  messagingSenderId: "393678004919",
-  appId: "1:393678004919:web:8bbb46c9751adc9e90e332",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const auth = getAuth(firebaseApp);
