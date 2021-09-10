fc69beffae7e84dbf4adad46a88088270583dd8c
Aryan Agrawal <ary4n99@gmail.com>
2021-09-10 13:41:45 +0200

    Improve countdown and add login button

--- src/components/landing/CountDownTimer.tsx
+++ src/components/landing/CountDownTimer.tsx
@@ -1,34 +1,21 @@
-import { useState } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Typography } from "@material-ui/core";
+import Countdown from "react-countdown";
 
 export default function CountDownTimer() {
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [mins, setMins] = useState(0);
-  const [secs, setSecs] = useState(0);
-
-  const countDownDate = new Date("Oct 15, 2021 00:00:00").getTime();
-
-  const myFunc = setInterval(function () {
-    const now = new Date().getTime();
-
-    const timeleft = countDownDate - now;
-
-    setDays(Math.floor(timeleft / (1000 * 60 * 60 * 24)));
-    setHours(Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    setMins(Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60)));
-    setSecs(Math.floor((timeleft % (1000 * 60)) / 1000));
-
-    if (timeleft < 0) {
-      clearInterval(myFunc);
-      setDays(0);
-      setHours(0);
-      setMins(0);
-      setSecs(0);
-    }
-  }, 1000);
-
-  const countDown = `${days} days ${hours} hours ${mins} mins ${secs} secs`;
-
-  return <Typography variant="h5">{countDown}</Typography>;
+  const renderer = ({ days, hours, minutes, seconds, completed }: any) => {
+    return (
+      <Typography variant="h5">
+        {completed
+          ? "Get Coding!"
+          : `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`}
+      </Typography>
+    );
+  };
+  return (
+    <Countdown
+      date={process.env.REACT_APP_HACKATHON_DATE}
+      renderer={renderer}
+    />
+  );
 }
