6a9102fa90024b213be867f197a355c66ecc65e8
Varun Shankar <varunshankar02@gmail.com>
2021-10-03 19:38:29 +0100

    Make changes to sponsors

--- src/components/landing/Sponsors.tsx
+++ src/components/landing/Sponsors.tsx
@@ -1,25 +1,47 @@
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import SponsorCategory from "src/pages/sponsors/SponsorCategory";
 import sponsors from "src/pages/sponsors/SponsorData.json";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyle = makeStyles(() => ({
+  root: {
+    backgroundColor: "#a59132",
+    color: "#fafdf6",
+    padding: "30px",
+  },
+  title: {
+    borderBottomStyle: "solid",
+    paddingBottom: "5px",
+  },
+  container: {
+    marginTop: "20px",
+  },
+}));
 
 export default function Sponsors() {
+  const classes = useStyle();
   return (
-    <Grid container>
-      <SponsorCategory
-        category="Gold"
-        colour="#AF9500"
-        sponsors={sponsors.gold}
-      />
-      <SponsorCategory
-        category="Silver"
-        colour="#B4B4B4"
-        sponsors={sponsors.silver}
-      />
-      <SponsorCategory
-        category="Bronze"
-        colour="#6A3805"
-        sponsors={sponsors.bronze}
-      />
-    </Grid>
+    <div id="sponsors" className={classes.root}>
+      <Typography variant="h4" align="center">
+        <a className={classes.title}>Meet our Sponsors</a>
+      </Typography>
+      <Grid container className={classes.container}>
+        <SponsorCategory
+          category="Gold"
+          colour="#AF9500"
+          sponsors={sponsors.gold}
+        />
+        <SponsorCategory
+          category="Silver"
+          colour="#B4B4B4"
+          sponsors={sponsors.silver}
+        />
+        <SponsorCategory
+          category="Bronze"
+          colour="#6A3805"
+          sponsors={sponsors.bronze}
+        />
+      </Grid>
+    </div>
   );
 }
